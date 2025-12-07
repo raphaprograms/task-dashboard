d6661739a4ba33bc44ae4c528c0c43ed74c8b070
raphaprograms <203123602+raphaprograms@users.noreply.github.com>
2025-12-07 11:38:06 -0800

    Create types

---
+++ src/features/tasks/types.ts
@@ -0,0 +1,33 @@
+export type TaskStatus = 'pending' | 'inprogress' | 'completed';
+
+export type TaskPriority = 'low' | 'medium' | 'high';
+
+export type Task = {
+    id: string;
+    title: string;
+    description: string;
+    status: TaskStatus;
+    priority: TaskPriority;
+    cratedAt: string;
+    updatedAt: string;
+};
+
+export type TaskFormData = {
+    title: string;
+    description: string;
+    priority: TaskPriority;
+};
+
+export type TaskFilter = {
+    status: TaskStatus | 'all';
+    priority: TaskPriority | 'all';
+    searchQuery: string;
+};
+
+export type TaskSortField = 'createdAt' | 'updatedAt' | 'priority' | 'title';
+export type TaskSortOrder = 'asc' | 'desc';
+
+export type TaskSort = { 
+    field: TaskSortField;
+    order: TaskSortOrder;
+}
